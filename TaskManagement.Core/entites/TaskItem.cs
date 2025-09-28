bd55cbe9d4301d2f712663a50bb7c8b3dc4e8d64
Nada Zaki <nada.zaki@devhouse-int.com>
2025-09-28 18:28:05 +0300

    Add project structure

---
+++ TaskManagement.Core/entites/TaskItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagement.Core.entites
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Status { get; set; } = "Pending";
+        public DateTime DueDate { get; set; }
+        public Guid UserId { get; set; }
+       // public User User { get; set; }
+    }
+}
